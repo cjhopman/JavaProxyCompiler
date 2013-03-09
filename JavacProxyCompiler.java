a16cd454caf6590541831d1f4b32bf5fc8f9c630
Chris Hopman <cjhopman@gmail.com>
2013-03-08 20:33:24 -0800

    .

--- JavacProxyCompiler.java
+++ JavacProxyCompiler.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.net.ServerSocket;
 import java.util.List;
 
 import javax.tools.JavaCompiler;
@@ -8,12 +9,14 @@ import javax.tools.JavaFileObject;
 import javax.tools.Diagnostic;
 import javax.tools.DiagnosticCollector;
 
+
 import py4j.GatewayServer;
 
 class JavacProxyCompiler {
   public static final String COMPILE_END_SIGNAL = "-----COMPILE FINISHED-----";
   public static final String READY_SIGNAL = "-----COMPILER READY-----";
   public static final String DEAD_SIGNAL = "-----COMPILER DIED-----";
+  public static final String PORT_SIGNAL = "-----PORT: %d-----";
 
   public static Boolean compile(List<String> options, List<String> java_files) throws IOException {
     JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
@@ -33,15 +36,20 @@ class JavacProxyCompiler {
     System.out.flush();
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     Runtime.getRuntime().addShutdownHook(new Thread() {
       public void run() {
         sendMessage(DEAD_SIGNAL);
       }
     });
 
-    GatewayServer gatewayServer = new GatewayServer(null);
-    gatewayServer.start(false);
+    ServerSocket socket = new ServerSocket(0);
+    int port = socket.getLocalPort();
+    socket.close();
+
+    GatewayServer gatewayServer = new GatewayServer(null, port);
+    gatewayServer.start();
     sendMessage(READY_SIGNAL);
+    sendMessage(String.format(PORT_SIGNAL, port));
   }
 }
--- javac_proxy_server.py
+++ javac_proxy_server.py
@@ -5,12 +5,13 @@ import pexpect
 import sys
 import time
 
-from py4j.java_gateway import JavaGateway
-from py4j.java_collections import ListConverter
+from py4j import java_gateway
+from py4j import java_collections
 
-COMPILE_END_SIGNAL = "-----COMPILE FINISHED-----"
-READY_SIGNAL = "-----COMPILER READY-----"
 DEAD_SIGNAL = "-----COMPILER DIED-----"
+READY_SIGNAL = "-----COMPILER READY-----"
+PORT_SIGNAL = "-----PORT: ([0-9]*)-----"
+COMPILE_END_SIGNAL = "-----COMPILE FINISHED-----"
 
 class CompilerProcess(object):
   def __init__(self, port):
@@ -18,39 +19,42 @@ class CompilerProcess(object):
     java_cmd = ['java', '-classpath', 'py4j0.7.jar:out', 'JavacProxyCompiler']
     self.process = pexpect.spawn(' '.join(java_cmd))
     self.process.logfile = sys.stdout
-    self.ready = False
 
-  def WaitForReady(self):
-    idx = self.process.expect([READY_SIGNAL, DEAD_SIGNAL])
-    if idx == 1:
-      self.Kill()
+    self.Expect(READY_SIGNAL)
+    match = self.Expect(PORT_SIGNAL)
+    self.port = int(match.group(1))
+
+    self.gateway_client = java_gateway.GatewayClient(port=self.port)
+    self.gateway = java_gateway.JavaGateway(gateway_client=self.gateway_client)
+
+
+  def Expect(self, signal):
+    idx = self.process.expect([DEAD_SIGNAL, signal])
+    if idx == 0:
+      self.Kill('Got DEAD_SIGNAL. Expected: ' + signal)
+    return self.process.match
+
 
   def Kill(self):
     raise Exception('Dead')
 
-  def SanityCheck(self):
-    pass
-
 
   def Compile(self, options, files):
-    self.WaitForReady()
-    self.SanityCheck()
-    files = ListConverter().convert(files, self.gateway._gateway_client)
-    options = ListConverter().convert(options)
-    success = gateway.jvm.JavacProxyCompiler.compile(options, files)
-    while True:
-      output = self.process.stdout.readline()
-      if output == COMPILE_END_SIGNAL:
-        break
-      print output
-    print self.process.stdout.read()
-    print success
+    files = java_collections.ListConverter().convert(files, self.gateway._gateway_client)
+    options = java_collections.ListConverter().convert(options, self.gateway._gateway_client)
+    success = self.gateway.jvm.JavacProxyCompiler.compile(options, files)
+    idx = self.process.expect([COMPILE_END_SIGNAL, DEAD_SIGNAL])
+    if idx == 1:
+      self.Kill()
+      return
+
+    print '!!!'
 
 
 def main(argv):
   compiler = CompilerProcess(0)
-  compiler.Compile(['-classpath', 'out/Jar.jar'], ['JarUse.java'])
-  compiler.compile()
+  options = argv[1:]
+  compiler.Compile(['-classpath', 'out/Jar.jar', '-d', 'out'], ['JarUse.java'])
 
 
 if __name__ == '__main__':
